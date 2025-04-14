c80fa3edd25b18c49903a4f2ad23597f3e5860e8
TioNicholas99 <166321884+TioNicholas99@users.noreply.github.com>
2025-04-14 22:50:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -280,3 +280,16 @@ document.addEventListener('DOMContentLoaded', function() {
     window.addEventListener('load', adjustVideoContainerHeight);
     window.addEventListener('resize', adjustVideoContainerHeight);
 });
+// Nonaktifkan tombol F12, Ctrl+Shift+I, Ctrl+U
+  document.addEventListener('keydown', function(e) {
+    if (
+      e.key === 'F12' ||
+      (e.ctrlKey && e.shiftKey && (e.key === 'I' || e.key === 'J')) ||
+      (e.ctrlKey && e.key === 'U')
+    ) {
+      e.preventDefault();
+    }
+  });
+
+  document.getElementById("konten").innerHTML = atob("SGFsbzogSmlrYSBhZGFsYWggdGFtcGlsIGxpaGF0Lg==");
+
